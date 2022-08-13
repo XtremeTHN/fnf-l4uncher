34e253b64b19a979dab47761087b469329ff229c
XtremeTHN <102498675+XtremeTHN@users.noreply.github.com>
2022-08-13 14:14:40 -0500

    Update version.py

--- updates/version.py
+++ updates/version.py
@@ -1,1 +1,1 @@
-files = {"py_config.py":"https://raw.githubusercontent.com/XtremeTHN/fnf-l4uncher/main/modules/py_config.py"}
+{"py_config.py":"https://raw.githubusercontent.com/XtremeTHN/fnf-l4uncher/main/modules/py_config.py"}
